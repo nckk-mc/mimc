a7e319d37f80056a7d9447794c6c4a4cdc322a7c
nckk-mc <nckk-mc@myicts.be>
2019-08-04 03:34:41 +0200

    Allows to remote debug paper on port 5100

--- start_node_spawn.sh
+++ start_node_spawn.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 cd Paper/work/test-server
-java -jar ../../Paper-Server/target/paper-1.14.4.jar -p 25565
+
+port=25565
+jar="../../Paper-Server/target/paper-1.14.4.jar"
+baseargs="-server -Xms${PAPER_MIN_TEST_MEMORY:-512M} -Xmx${PAPER_TEST_MEMORY:-2G} -Dfile.encoding=UTF-8 -XX:MaxGCPauseMillis=150 -XX:+UseG1GC "
+baseargs="$baseargs -DIReallyKnowWhatIAmDoingISwear=1 -XX:TargetSurvivorRatio=90 "
+baseargs="$baseargs -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=40 -XX:G1MaxNewSizePercent=80 "
+baseargs="$baseargs -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=20 "
+baseargs="$baseargs -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5100"
+cmd="java ${PAPER_TEST_BASE_JVM_ARGS:-$baseargs} -jar $jar -p $port"
+/usr/bin/env bash -c "$cmd"
