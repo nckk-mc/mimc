5eca2b6f850074646be4b02e8683abf08a9f2337
nckk-mc <nckk-mc@myicts.be>
2019-08-04 14:33:15 +0200

    Syntax error for linux clients

--- setup.sh
+++ setup.sh
@@ -43,12 +43,12 @@ cd ../..
 
 echo "Applying paper-patches"
 cd Paper-Server
-for patch in ../../paper-patches/*.patch; do git apply $patch done
+for patch in ../../paper-patches/*.patch; do git apply $patch; done
 cd ..
 
 echo "Applying paper-api-patches"
 cd Paper-Api
-for patch in ../../paper-api-patches/*.patch; do git apply $patch done
+for patch in ../../paper-api-patches/*.patch; do git apply $patch; done
 cd ..
 
 echo "Rebuilding..."
@@ -68,7 +68,7 @@ cd waterfall
 
 echo "Applying waterfall-patches"
 cd Waterfall-Proxy
-for patch in ../../waterfall-patches/*.patch; do git apply $patch done
+for patch in ../../waterfall-patches/*.patch; do git apply $patch; done
 cd ..
 	
 echo "MIMC | Building..."
