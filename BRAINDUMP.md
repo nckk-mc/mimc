337a88ef2e596a71178e64d8f6af8870563c9e9a
nckk-mc <nckk-mc@myicts.be>
2019-08-20 03:15:57 +0200

    Adds support for portal teleportation across nodes

--- BRAINDUMP.md
+++ BRAINDUMP.md
@@ -23,3 +23,14 @@
 
 ### The proxy could keep track of loaded region files
 - If we have the nodes communicate their loaded/unloaded region files back to the proxy it could make sure we never load the same region files on two nodes.
+
+### There are a number of uses of `worldserver.getSpawn()`
+- Identify them, and neutralize or disable
+- We cannot have two servers loading the same chunks
+
+
+### Support for /locate requires us to increase MERGE_DISTANCE
+- see https://www.spigotmc.org/threads/how-to-find-closest-generated-structure.338532/
+- /locate has a 100 chunk limit (1.6k blocks) which, when allowed, requires us to increase MERGE_DISTANCE to 4k
+- Perhaps we could make support for this opt-in, increasing the minimal MERGE_DISTANCE
+
--- paper-plugins/migrate/src/mimc/MigratePlugin.java
+++ paper-plugins/migrate/src/mimc/MigratePlugin.java
@@ -3,14 +3,20 @@ package mimc;
 import com.google.common.io.ByteArrayDataInput;
 import com.google.common.io.ByteArrayDataOutput;
 import com.google.common.io.ByteStreams;
+import mimc.event.player.PlayerNonLocalTeleportEvent;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.world.ChunkLoadEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.plugin.messaging.PluginMessageListener;
 
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArraySet;
 
@@ -36,15 +42,38 @@ public class MigratePlugin extends JavaPlugin implements PluginMessageListener, 
         Player p = e.getPlayer();
         String playerName = p.getName();
         playersQueuedForMigration.remove(playerName);
+        playersQueuedForNonLocalTeleport.remove(playerName);
     }
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent e) {
         Player p = e.getPlayer();
         String playerName = p.getName();
-        if (this.playersQueuedForMigration.contains(playerName)) {
+        if (playersQueuedForMigration.contains(playerName)
+            && !playersQueuedForNonLocalTeleport.contains(playerName)) {
             MimcServer server = (MimcServer)getServer();
             server.savePlayerData(p.getName());
         }
+    }
+    Set<String> playersQueuedForNonLocalTeleport = new CopyOnWriteArraySet<>();
+    @EventHandler
+    public void onPlayerNonLocalTeleport(PlayerNonLocalTeleportEvent e) {
+        Player p = e.getPlayer();
+        String playerName = p.getName();
+        playersQueuedForNonLocalTeleport.add(playerName);
+
+        ByteArrayDataOutput out = ByteStreams.newDataOutput();
+        out.writeUTF("non-local-teleport");
+        out.writeUTF(playerName);
+        out.writeInt(e.toDimension);
+        out.writeDouble(e.getTo().getX());
+        out.writeDouble(e.getTo().getY());
+        out.writeDouble(e.getTo().getZ());
+        out.writeFloat(e.getTo().getYaw());
+        out.writeFloat(e.getTo().getPitch());
+
+        getLogger().warning(String.format( "Non-local teleport request sent to bungee for player %s", playerName ));
+
+        p.sendPluginMessage(this, CHANNEL_NAME, out.toByteArray());
     }
 
     @Override
@@ -87,6 +116,31 @@ public class MigratePlugin extends JavaPlugin implements PluginMessageListener, 
 
                 player.sendPluginMessage(this, CHANNEL_NAME, out.toByteArray());
             }
+        } else if (subchannel.equalsIgnoreCase("confirm-teleport")) {
+            int dimension = in.readInt();
+            double x = in.readDouble();
+            double y = in.readDouble();
+            double z = in.readDouble();
+            float yaw = in.readFloat();
+            float pitch = in.readFloat();
+
+            World world = getServer().getWorld(mapDimension(dimension));
+            Location location = new Location(world, x, y, z, yaw, pitch);
+
+            getLogger().warning(String.format( "Teleporting player '%s' to %s", player.getName(), location));
+
+            player.teleport(location);
+
+            MimcServer server = (MimcServer)getServer();
+            server.setPlayerReadOnly(player.getName(), false);
         }
+    }
+
+    String mapDimension(int dimension) {
+        switch (dimension) {
+            case -1: return "world_nether";
+            case 1: return "world_the_end";
+        }
+        return "world";
     }
 }
